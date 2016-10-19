5c07b14a9266d3cf6e2e0602744d60c69e2e8e2a
sapphire <sapphiredev@users.noreply.github.com>
2016-10-19 14:18:24 +0900

    refresh timeline

--- hagikaze/TimelineViewController.swift
+++ hagikaze/TimelineViewController.swift
@@ -12,6 +12,9 @@ import Kingfisher
 
 class TimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 	@IBOutlet weak var tableView: UITableView!
+	var refreshControl: UIRefreshControl!
+	
+	var client: TWTRAPIClient?
 	
 	var tweets: [TWTRTweet] = [] {
 		didSet {
@@ -22,28 +25,46 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+		refreshControl = UIRefreshControl()
+		refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+		tableView.addSubview(refreshControl)
+		
 		if let userID = Twitter.sharedInstance().sessionStore.session()?.userID {
-			let client = TWTRAPIClient(userID: userID)
-			let parameters: [String: String] = [:]
-			let request = client.urlRequest(withMethod: "GET", url: "https://api.twitter.com/1.1/statuses/home_timeline.json", parameters: parameters, error: nil)
-			client.sendTwitterRequest(request, completion: { (res, data, err) in
-				if err != nil {
-					print(err)
-				}
-				
-				do {
-					let json: Any = try JSONSerialization.jsonObject(with: data!, options: [JSONSerialization.ReadingOptions.mutableContainers])
-					if let jsonArray = json as? NSArray {
-						let tweets = TWTRTweet.tweets(withJSONArray: jsonArray as? [Any])
-						self.tweets = tweets as! [TWTRTweet]
-					}
-				}
-				catch let jsonError as NSError {
-					print("json error: \(jsonError.localizedDescription)")
-				}
-			})
+			client = TWTRAPIClient(userID: userID)
+			
+			fetchHomeTimeline()
 		}
     }
+
+	func fetchHomeTimeline() {
+		var parameters: [String: String] = [:]
+		if self.tweets.count > 0 {
+			parameters["since_id"] = self.tweets.first?.tweetID
+		}
+		let request = client?.urlRequest(withMethod: "GET", url: "https://api.twitter.com/1.1/statuses/home_timeline.json", parameters: parameters, error: nil)
+		client?.sendTwitterRequest(request!, completion: { (res, data, err) in
+			if err != nil {
+				print(err)
+			}
+			
+			do {
+				let json: Any = try JSONSerialization.jsonObject(with: data!, options: [JSONSerialization.ReadingOptions.mutableContainers])
+				if let jsonArray = json as? NSArray {
+					var tweets = TWTRTweet.tweets(withJSONArray: jsonArray as? [Any]) as! [TWTRTweet]
+					tweets.append(contentsOf: self.tweets)
+					self.tweets = tweets
+				}
+			}
+			catch let jsonError as NSError {
+				print("json error: \(jsonError.localizedDescription)")
+			}
+		})
+	}
+
+	func refresh() {
+		fetchHomeTimeline()
+		refreshControl.endRefreshing()
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -58,21 +79,27 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "timelineTweet", for: indexPath)
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTweet", for: indexPath)
 		
 		let tweet = tweets[indexPath.row]
 		
+		let profileImageView = cell.viewWithTag(100) as! UIImageView?
+		let screenNameLabel = cell.viewWithTag(101) as! UILabel?
+		let textLabel = cell.viewWithTag(102) as! UILabel?
+		
 		let url = URL(string: tweet.author.profileImageURL)
-		cell.imageView?.kf.setImage(with: url, placeholder: nil, options: nil, progressBlock: nil, completionHandler: { (image, error, cacheType, imageUrl) in
+		profileImageView?.kf.setImage(with: url, placeholder: nil, options: nil, progressBlock: nil, completionHandler: { (image, error, cacheType, imageUrl) in
 			let cell = tableView.cellForRow(at: indexPath)
 			if cell != nil {
 				tableView.reloadRows(at: [indexPath], with: .automatic)
+				profileImageView?.contentMode = UIViewContentMode.scaleAspectFit
 			}
 		})
 		
-		cell.textLabel?.text = tweet.text
-		cell.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
-		cell.textLabel?.numberOfLines = 0
+		screenNameLabel?.text = "@" + tweet.author.screenName
+		
+		textLabel?.text = tweet.text
+		textLabel?.sizeToFit()
 		
 		return cell
 	}
